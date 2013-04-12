9df69973f73b9f9bc2aeba799453d79feb736416
goten4 <goten4@gmail.com>
2013-04-12 23:50:12 +0200

    Fix phpunit tests config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,11 @@ if (! isset($loader)) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
+define('ZF_PERSISTENCE_BASE_PATH', dirname(dirname(__DIR__)) . '/ZfPersistenceBase');
+
 /* @var $loader \Composer\Autoload\ClassLoader */
+$loader->add('ZfPersistenceBase\\', ZF_PERSISTENCE_BASE_PATH . '/src');
+$loader->add('ZfPersistenceBaseTest\\', ZF_PERSISTENCE_BASE_PATH . '/tests');
 $loader->add('ZfPersistenceZendDb\\', dirname(__DIR__) . '/src');
 $loader->add('ZfPersistenceZendDbTest\\', __DIR__);
 unset($files, $file, $loader);
