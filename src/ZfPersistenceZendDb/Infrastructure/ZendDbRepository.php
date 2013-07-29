16bd8c8a311fb7d6d74444ffd70822cff6eb0790
goten4 <goten4@gmail.com>
2013-07-29 15:21:38 +0200

    FIX ZendDbRepository

--- src/ZfPersistenceZendDb/Infrastructure/ZendDbRepository.php
+++ src/ZfPersistenceZendDb/Infrastructure/ZendDbRepository.php
@@ -1,11 +1,14 @@
 <?php
 namespace ZfPersistenceZendDb\Infrastructure;
 
-use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\Adapter\Platform\Sqlite;
 
 use Zend\Db\Sql\Sql;
-use Zend\Stdlib\Hydrator\ArraySerializable;
+use Zend\Db\Sql\Expression as SqlExpression;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\ResultSet\HydratingResultSet;
+use Zend\Stdlib\Hydrator\ArraySerializable;
 use ZfPersistenceZendDb\Db\Adapter\MasterSlavesAdapterInterface;
 use ZfPersistenceBase\Model\AggregateRoot;
 use ZfPersistenceBase\Model\Repository;
@@ -25,14 +28,23 @@ abstract class ZendDbRepository implements Repository
 
     public function add(AggregateRoot $aggregateRoot)
     {
+        $insert = $this->getMasterSql()->insert($this->tableName())->values($aggregateRoot->getArrayCopy());
+        $this->getMasterSql()->prepareStatementForSqlObject($insert)->execute();
     }
 
     public function getAll()
     {
+        return $this->executeSelect($this->getSelect());
     }
 
     public function size()
     {
+        $resultSet = new ResultSet();
+        $select = $this->getSelect()->columns(array(
+            'size' => new SqlExpression('COUNT(*)')
+        ));
+        $resultSet->initialize($this->getSlaveSql()->prepareStatementForSqlObject($select)->execute());
+        return $resultSet->current()->size;
     }
 
     public function getById($id)
@@ -44,37 +56,54 @@ abstract class ZendDbRepository implements Repository
 
     public function update(AggregateRoot $aggregateRoot)
     {
+        $update = $this->getMasterSql()->update($this->tableName())->set($aggregateRoot->getArrayCopy())->where(
+            array(
+                $this->id() => $aggregateRoot->getId()
+            ));
+        $this->getMasterSql()->prepareStatementForSqlObject($update)->execute();
     }
 
     public function remove(AggregateRoot $aggregateRoot)
     {
+        $delete = $this->getMasterSql()->delete($this->tableName())->where(
+            array(
+                $this->id() => $aggregateRoot->getId()
+            ));
+        $this->getMasterSql()->prepareStatementForSqlObject($delete)->execute();
     }
 
     public function removeAll(array $aggregateRoots = NULL)
     {
+        $delete = $this->getMasterSql()->delete($this->tableName());
+        if ($aggregateRoots) {
+            $ids = array_map(function (AggregateRoot $aggregateRoot) { return $aggregateRoot->getId(); }, $aggregateRoots);
+            $delete->where->in($this->id(), $ids);
+        }
+        $this->getMasterSql()->prepareStatementForSqlObject($delete)->execute();
     }
 
     protected function getAllBy($criteria)
     {
         return $this->executeSelect($this->getSelect()->where($criteria));
     }
-    
+
     protected function getBy($criteria)
     {
         $aggregateRoots = $this->getAllBy($criteria);
-		return empty($aggregateRoots) ? null : $aggregateRoots[0];
+        return empty($aggregateRoots) ? null : $aggregateRoots[0];
     }
 
     protected function getSelect()
     {
         return $this->getSlaveSql()->select()->from($this->tableName());
     }
-    
+
     protected function executeSelect($select)
     {
         $statement = $this->getSlaveSql()->prepareStatementForSqlObject($select);
         $result = $statement->execute();
-        $resultSet = new HydratingResultSet($this->getHydrator(), new $this->aggregateRootClassName());
+        $className = $this->aggregateRootClassName();
+        $resultSet = new HydratingResultSet($this->getHydrator(), new $className());
         $resultSet->initialize($result);
         $aggregateRoots = array();
         foreach ($resultSet as $aggregateRoot) {
@@ -102,13 +131,13 @@ abstract class ZendDbRepository implements Repository
     {
         return $this->getDbAdapter()->getSlaveAdapter();
     }
-    
+
     public function setDbAdapter(MasterSlavesAdapterInterface $dbAdapter)
     {
         $this->dbAdapter = $dbAdapter;
         return $this;
     }
-    
+
     protected function getMasterSql()
     {
         return $this->masterSql;
@@ -132,5 +161,6 @@ abstract class ZendDbRepository implements Repository
     }
 
     protected abstract function tableName();
+
     protected abstract function aggregateRootClassName();
 }
--- tests/ZfPersistenceZendDbTest/Infrastructure/ZendDbUserRepositoryTest.php
+++ tests/ZfPersistenceZendDbTest/Infrastructure/ZendDbUserRepositoryTest.php
@@ -8,118 +8,106 @@ class ZendDbUserRepositoryTest extends \PHPUnit_Extensions_Database_TestCase
 {
     protected static $serviceManager;
     protected static $connection;
-    protected $repository;
-    
+    protected static $repository;
+
     public static function setUpBeforeClass()
     {
         static::$serviceManager = ServiceManagerFactory::getServiceManager();
         static::$connection = static::$serviceManager->get('Zend\Db\Adapter\Adapter')->getDriver()->getConnection()->getResource();
-		static::$connection->exec(file_get_contents(TEST_BASE_PATH . '/data/schema.sql'));
-    }
-    
-    protected function setUp()
-    {
-        $this->repository = static::$serviceManager->get('ZfPersistence\UserRepository');
+        static::$connection->exec(file_get_contents(TEST_BASE_PATH . '/data/schema.sql'));
+        static::$repository = static::$serviceManager->get('ZfPersistence\UserRepository');
     }
 
     public function getConnection()
     {
-		return $this->createDefaultDBConnection(static::$connection);
+        return $this->createDefaultDBConnection(static::$connection);
     }
 
     public function getDataSet()
     {
         return $this->createFlatXMLDataSet(TEST_BASE_PATH . '/data/fixtures.xml');
     }
-    
+
+    /** @test */
+    public function canGetSize()
+    {
+        $this->assertEquals(3, static::$repository->size());
+    }
+
     /** @test */
     public function canAdd()
     {
-        $this->repository->add(new User('Bill'));
-    
-        $this->assertEquals(4, $this->repository->size());
+        static::$repository->add(new User('Bill'));
+        
+        $this->assertEquals(4, static::$repository->size());
     }
-    
+
     /** @test */
     public function cannotGetByUnknownId()
     {
-        $this->assertNull($this->repository->getById(99));
+        $this->assertNull(static::$repository->getById(99));
     }
-    
+
     /** @test */
     public function canGetById()
     {
-        $storedAggregateRoot = $this->repository->getById(1);
-    
+        $storedAggregateRoot = static::$repository->getById(1);
+        
         $this->assertInstanceOf('ZfPersistenceBaseTest\Model\User', $storedAggregateRoot);
         $this->assertEquals('John', $storedAggregateRoot->getName());
     }
-    
+
     /** @test */
     public function canGetAll()
     {
-        $aggregateRoots = $this->repository->getAll();
-    
+        $aggregateRoots = static::$repository->getAll();
+        
         $this->assertInternalType('array', $aggregateRoots);
         $this->assertEquals(3, count($aggregateRoots));
         $this->assertInstanceOf('ZfPersistenceBaseTest\Model\User', $aggregateRoots[0]);
     }
-    
-    /** @test */
-    public function canGetSize()
-    {
-        $this->assertEquals(3, $this->repository->size());
-    }
-    
+
     /** @test */
     public function canUpdate()
     {
-        $aggregateRoot = $this->getAggregateRootById(1)->setName('Jack');
-    
-        $this->repository->update($aggregateRoot);
-    
-        $this->assertEquals('Jack', $this->getAggregateRootById(1)->getName());
+        $aggregateRoot = static::$repository->getById(1)->setName('Jack');
+        
+        static::$repository->update($aggregateRoot);
+        
+        $this->assertEquals('Jack', static::$repository->getById(1)->getName());
     }
-    
+
     /** @test */
     public function canRemove()
     {
-        $aggregateRoot = $this->getAggregateRootById(1);
-    
-        $this->repository->remove($aggregateRoot);
-    
-        $this->assertEquals(2, $this->repository->size());
-        $this->assertNull($this->repository->getById(1));
+        $aggregateRoot = static::$repository->getById(1);
+        
+        static::$repository->remove($aggregateRoot);
+        
+        $this->assertEquals(2, static::$repository->size());
+        $this->assertNull(static::$repository->getById(1));
     }
-    
+
     /** @test */
     public function canRemoveAllSpecifiedAggregateRoots()
     {
-        $aggregateRoot1 = $this->getAggregateRootById(1);
-        $aggregateRoot2 = $this->getAggregateRootById(2);
-        $this->repository->removeAll(array(
-            $aggregateRoot1, $aggregateRoot2
+        $aggregateRoot1 = static::$repository->getById(1);
+        $aggregateRoot2 = static::$repository->getById(2);
+        static::$repository->removeAll(array(
+            $aggregateRoot1, 
+            $aggregateRoot2
         ));
-    
-        $this->assertEquals(1, $this->repository->size());
-        $this->assertNull($this->repository->getById(1));
-        $this->assertNull($this->repository->getById(2));
+        
+        $this->assertEquals(1, static::$repository->size());
+        $this->assertNull(static::$repository->getById(1));
+        $this->assertNull(static::$repository->getById(2));
     }
-    
+
     /** @test */
     public function canRemoveAllAggregateRoots()
     {
-        $this->repository->removeAll();
-    
-        $this->assertEquals(0, $this->repository->size());
-    }
-    
-    private function getAggregateRootById($id)
-    {
-        $aggregateRoot = $this->repository->getById($id);
-        if ($aggregateRoot == null) {
-            $this->fail('Method getById() failed');
-        }
-        return $aggregateRoot;
+        static::$repository->removeAll();
+        
+        $this->assertEquals(0, static::$repository->size());
     }
 }
